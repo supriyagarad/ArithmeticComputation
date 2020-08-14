d7ba29494664eb9994d3275ec72b2b95e6a92aac
supriyagarad <garadsupriya1526@gmail.com>
2020-08-14 22:27:15 +0530

    Added rseult in Dictionary

--- Arithmetic.sh
+++ Arithmetic.sh
@@ -2,6 +2,7 @@
 
 echo "computes different arithmetic expression"
 
+#input from user
 read -p "enter number:" a
 read -p "enter number:" b
 read -p "enter number:" c
@@ -11,3 +12,17 @@ result1=$(($a+$b*$c))
 result2=$(($a*$b+$c))
 result3=$(($c+$a/$b)) 
 result4=$(($a%$b+$c))
+
+#create dictionary
+
+#create Dictionary
+
+declare -A dictionary
+dictionary[1]=$result1
+dictionary[2]=$result2
+dictionary[3]=$result3
+dictionary[4]=$result4
+
+echo "${!dictionary[@]}"
+echo "${dictionary[@]}"
+
